1a7c40c88d677992a0cf66ea92eb1d8b9a94aaad
GPA <gpalskis@gmail.com>
2023-08-30 18:29:50 +0300

    syntax corr

--- IaC-tf/aks.tf
+++ IaC-tf/aks.tf
@@ -18,12 +18,12 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 }
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.gpatfaks1.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.gpatfaks1.kube_config_raw
 
   sensitive = true
 }
