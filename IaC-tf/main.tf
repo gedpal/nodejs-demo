444a0b72652742f32f86d0a8be46b5c0f0f3fe0d
GPA <gpalskis@gmail.com>
2023-08-30 17:48:48 +0300

    req providers

--- IaC-tf/main.tf
+++ IaC-tf/main.tf
@@ -1,8 +1,15 @@
-required_providers {
-  azurerm = {
-    source  = "hashicorp/azurerm"
-    version = ">=3.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
   }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
 }
 resource "azurerm_resource_group" "aks_rg" {
   name     = "Gediminas_Palskis_rg"
