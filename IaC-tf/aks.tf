ec21cac642f04c44e0362382b943bca47b015aca
GPA <gpalskis@gmail.com>
2023-08-30 17:52:58 +0300

    req providers

--- IaC-tf/aks.tf
+++ IaC-tf/aks.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = "gpatfaks1"
   location            = azurerm_resource_group.aks_rg.location
