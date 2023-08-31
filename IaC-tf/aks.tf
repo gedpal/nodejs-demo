4fe00ea5696d75440da136d781f8964ba4d0928c
GPA <gpalskis@gmail.com>
2023-08-31 14:50:59 +0300

    vm: Standard_D2_v2

--- IaC-tf/aks.tf
+++ IaC-tf/aks.tf
@@ -7,7 +7,8 @@ resource "azurerm_kubernetes_cluster" "gpatfaks1" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B2als_v2"
+#    vm_size    = "Standard_B2als_v2" <- hit limits :)
+    vm_size    = Standard_D2_v2
   }
 
   identity {
