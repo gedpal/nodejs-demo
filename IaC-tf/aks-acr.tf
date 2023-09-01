47723732d90e248bb6dc6daac6bd779504f94bbb
GPA <gpalskis@gmail.com>
2023-09-01 08:52:23 +0300

    syntax

--- IaC-tf/aks-acr.tf
+++ IaC-tf/aks-acr.tf
@@ -36,8 +36,8 @@ resource "azurerm_container_registry" "gpatfacr1" {
   admin_enabled            = false
 }
 resource "azurerm_role_assignment" "roleassign" {
-  principal_id                     =  azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id                     =  azurerm_kubernetes_cluster.gpatfaks1.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
+  scope                            = azurerm_container_registry.gpatfacr1.id
   skip_service_principal_aad_check = true
 }
