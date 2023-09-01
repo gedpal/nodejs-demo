d72e05d2a6cd3d9b690ecdd59910f6e172a6cf45
GPA <gpalskis@gmail.com>
2023-09-01 08:49:25 +0300

    ..

--- IaC-tf/main.tf
+++ IaC-tf/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 backend "azurerm" {
-      resource_group_name  = "'Gediminas_Palskis_rg'"
+      resource_group_name  = "Gediminas_Palskis_rg"
       storage_account_name = "gedpalsa1"
       container_name       = "tf-state"
       key                  = "tf/terraform.tfstate"
