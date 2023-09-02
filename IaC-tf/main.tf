83249c8e69c9174bacb48a524a49508afe9dcb70
GPA <gpalskis@gmail.com>
2023-09-02 15:10:07 +0300

    data use for rg

--- IaC-tf/main.tf
+++ IaC-tf/main.tf
@@ -38,12 +38,12 @@ resource "azurerm_virtual_network" "test" {
 data "azurerm_subnet" "kubesubnet" {
   name                 = var.aks_subnet_name
   virtual_network_name = azurerm_virtual_network.test.name
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
 }
 data "azurerm_subnet" "appgwsubnet" {
   name                 = "appgwsubnet"
   virtual_network_name = azurerm_virtual_network.test.name
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
 }
 
 # Public Ip 
