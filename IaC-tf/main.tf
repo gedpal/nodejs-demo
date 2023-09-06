7943324b4459e9667833b45555cac07919e4673b
GPA <gpalskis@gmail.com>
2023-09-06 15:31:17 +0300

    skip_service_principal_aad_check = true

--- IaC-tf/main.tf
+++ IaC-tf/main.tf
@@ -164,5 +164,5 @@ resource "azurerm_container_registry" "acr1" {
   principal_id                     =  azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr1.id
-#  skip_service_principal_aad_check = true
+  skip_service_principal_aad_check = true
 }
