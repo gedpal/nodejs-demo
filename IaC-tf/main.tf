4eee828a0c4fa12931fdf1fbce0b83010e57cbac
GPA <gpalskis@gmail.com>
2023-09-07 16:34:11 +0300

    due to Error: doesn't support update

--- IaC-tf/main.tf
+++ IaC-tf/main.tf
@@ -160,9 +160,9 @@ resource "azurerm_container_registry" "acr1" {
     type = "SystemAssigned"
   }
 }
- resource "azurerm_role_assignment" "roleassign" {
+/*  resource "azurerm_role_assignment" "roleassign" {
   principal_id                     =  azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr1.id
   skip_service_principal_aad_check = true
-}
+} */
